aead674c1f90de7763bcd0b0e13967f9766e77d3
JamiuKolawole04 <75920331+JamiuKolawole04@users.noreply.github.com>
2021-07-19 01:29:49 +0100

    Add files via upload
    initial commit

---
+++ Portfolio/mobileNav.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = '100%' ;
+    document.getElementById("mySidenav").style.backgroundColor = "#000"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = '0' ;
+}
+
+/*
+goTop.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+*/
+
+function start() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
